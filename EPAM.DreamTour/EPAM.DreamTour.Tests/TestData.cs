c3a5d7af3b419acee2e993cab2c3b21e62d51ec7
p0lich <a.polikarpov2014@gmail.com>
2022-01-25 21:38:42 +0400

    add tests for address logic

---
+++ EPAM.DreamTour/EPAM.DreamTour.Tests/TestData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPAM.DreamTour.Tests
+{
+    public static class TestData
+    {
+        public static async Task<IEnumerable<string>> GetCanadaTestRegions()
+        {
+            List<string> regions = new List<string>();
+            
+            regions.Add("Ontario");
+            regions.Add("British Columbia");
+            regions.Add("Manitoba");
+            regions.Add("Quebec");
+
+            return regions;
+        }
+
+        public static async Task<IEnumerable<string>> GetBritishColumbiaCities()
+        {
+            List<string> cities = new List<string>();
+
+            cities.Add("Burnaby");
+            cities.Add("Surrey");
+
+            return cities;
+        }
+
+        public static async Task<IEnumerable<string>> GetEmptyCollection()
+        {
+            return new List<string>();
+        }
+    }
+}
