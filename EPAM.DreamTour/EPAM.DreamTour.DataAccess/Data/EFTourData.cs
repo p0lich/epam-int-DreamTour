b7b3d78a768ac339bb1c76e2fb0c4f5d9f1ce39b
p0lich <a.polikarpov2014@gmail.com>
2022-02-15 01:36:00 +0400

    add EF core layer for data

---
+++ EPAM.DreamTour/EPAM.DreamTour.DataAccess/Data/EFTourData.cs
@@ -0,0 +1,71 @@
+﻿using EPAM.DreamTour.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM.DreamTour.DataAccess.Data
+{
+    public class EFTourData : ITourData
+    {
+        private TourContext _tourContext;
+
+        public EFTourData(TourContext tourContext)
+        {
+            _tourContext = tourContext;
+        }
+
+        public async Task Add(CreateTourModel tour)
+        {
+            //_tourContext.Tours.Add(tour);
+            await _tourContext.SaveChangesAsync();
+        }
+
+        public async Task<TourModel> Get(Guid id)
+        {
+            return await _tourContext.Tours.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TourModel>> GetBest()
+        {
+            return await _tourContext.Tours.OrderBy(t => Guid.NewGuid()).Take(5).ToListAsync();
+        }
+
+        public async Task<IEnumerable<GroupedTours>> GetCountryGroups()
+        {
+            return await _tourContext.Tours.Select(t => new GroupedTours()
+            {
+                Country = t.Country,
+                Count = _tourContext.Tours.Count(st => st.Country == t.Country)
+            }).ToListAsync();
+        }
+
+        public async Task<IEnumerable<string>> GetCountryRegions(string country)
+        {
+            return await _tourContext.Tours.Where(t => t.Country == country)
+                .Select(t => t.Region)
+                .Distinct()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TourModel>> GetFilteredTours(SearchRequest searchRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<string>> GetRegionCities(string region)
+        {
+            return await _tourContext.Tours.Where(t => t.Region == region)
+                .Select(t => t.Region)
+                .Distinct()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<TourModel>> GetTours()
+        {
+            return await _tourContext.Tours.ToListAsync();
+        }
+    }
+}
---
+++ EPAM.DreamTour/EPAM.DreamTour.DataAccess/Data/TourContext.cs
@@ -0,0 +1,15 @@
+﻿using EPAM.DreamTour.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPAM.DreamTour.DataAccess.Data
+{
+    public class TourContext : DbContext
+    {
+        public TourContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<TourModel> Tours { get; set; }
+    }
+}
