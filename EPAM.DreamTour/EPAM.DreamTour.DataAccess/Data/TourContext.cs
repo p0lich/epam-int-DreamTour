7eea4a8a1a630f148b0033cebea71b57d65a12ba
p0lich <a.polikarpov2014@gmail.com>
2022-02-15 15:05:57 +0400

    update TourContext

--- EPAM.DreamTour/EPAM.DreamTour.DataAccess/Data/TourContext.cs
+++ EPAM.DreamTour/EPAM.DreamTour.DataAccess/Data/TourContext.cs
@@ -6,10 +6,51 @@ using System.Text;
 
 namespace EPAM.DreamTour.DataAccess.Data
 {
-    public class TourContext : DbContext
+    public partial class TourContext : DbContext
     {
-        public TourContext(DbContextOptions options) : base(options) { }
+        public TourContext() { }
 
-        public DbSet<TourModel> Tours { get; set; }
+        public TourContext(DbContextOptions<TourContext> options) : base(options) { }
+
+        public DbSet<TourModel> Tour { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-83KP24G;Initial Catalog=DreamTourDb;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TourModel>(entity =>
+            {
+                entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.BeginDate).HasColumnType("date");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Country)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.District)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Region)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
