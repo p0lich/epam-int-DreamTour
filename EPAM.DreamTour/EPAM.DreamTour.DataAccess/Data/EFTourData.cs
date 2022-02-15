6833627038f0a373f85ad60dc1a83ee233b47f4d
p0lich <a.polikarpov2014@gmail.com>
2022-02-15 15:05:43 +0400

    add create method, filtered search method

--- EPAM.DreamTour/EPAM.DreamTour.DataAccess/Data/EFTourData.cs
+++ EPAM.DreamTour/EPAM.DreamTour.DataAccess/Data/EFTourData.cs
@@ -19,32 +19,42 @@ namespace EPAM.DreamTour.DataAccess.Data
 
         public async Task Add(CreateTourModel tour)
         {
-            //_tourContext.Tours.Add(tour);
+            _tourContext.Tour.Add(new TourModel()
+            {
+                Country = tour.Country,
+                Region = tour.Region,
+                District = tour.District,
+                City = tour.City,
+                MinimalPrice = tour.MinimalPrice,
+                MaximumPrice = tour.MaximumPrice,
+                DaysCount = tour.DaysCount,
+                BeginDate = tour.BeginDate
+            });
             await _tourContext.SaveChangesAsync();
         }
 
         public async Task<TourModel> Get(Guid id)
         {
-            return await _tourContext.Tours.FindAsync(id);
+            return await _tourContext.Tour.FindAsync(id);
         }
 
         public async Task<IEnumerable<TourModel>> GetBest()
         {
-            return await _tourContext.Tours.OrderBy(t => Guid.NewGuid()).Take(5).ToListAsync();
+            return await _tourContext.Tour.OrderBy(t => Guid.NewGuid()).Take(3).ToListAsync();
         }
 
         public async Task<IEnumerable<GroupedTours>> GetCountryGroups()
         {
-            return await _tourContext.Tours.Select(t => new GroupedTours()
+            return await _tourContext.Tour.Select(t => new GroupedTours()
             {
                 Country = t.Country,
-                Count = _tourContext.Tours.Count(st => st.Country == t.Country)
-            }).ToListAsync();
+                Count = _tourContext.Tour.Count(st => st.Country == t.Country)
+            }).Distinct().ToListAsync();
         }
 
         public async Task<IEnumerable<string>> GetCountryRegions(string country)
         {
-            return await _tourContext.Tours.Where(t => t.Country == country)
+            return await _tourContext.Tour.Where(t => t.Country == country)
                 .Select(t => t.Region)
                 .Distinct()
                 .ToListAsync();
@@ -52,12 +62,22 @@ namespace EPAM.DreamTour.DataAccess.Data
 
         public async Task<IEnumerable<TourModel>> GetFilteredTours(SearchRequest searchRequest)
         {
-            throw new NotImplementedException();
+            return await _tourContext.Tour
+                .Where(t =>
+                    (t.MinimalPrice >= searchRequest.MinimalPrice || !searchRequest.MinimalPrice.HasValue) &&
+                    (t.MaximumPrice <= searchRequest.MaximumPrice || !searchRequest.MaximumPrice.HasValue) &&
+                    (t.DaysCount == searchRequest.DaysCount || !searchRequest.DaysCount.HasValue) &&
+                    (t.BeginDate == searchRequest.BeginDate || !searchRequest.BeginDate.HasValue) &&
+                    (EF.Functions.Like(t.Country, $"%{searchRequest.Country}%") || string.IsNullOrEmpty(searchRequest.Country)) &&
+                    (EF.Functions.Like(t.Region, $"%{searchRequest.Region}%") || string.IsNullOrEmpty(searchRequest.Region)) &&
+                    (EF.Functions.Like(t.District, $"%{searchRequest.District}%") || string.IsNullOrEmpty(searchRequest.District)) &&
+                    (EF.Functions.Like(t.City, $"%{searchRequest.City}%") || string.IsNullOrEmpty(searchRequest.City)))
+                .ToListAsync();
         }
 
         public async Task<IEnumerable<string>> GetRegionCities(string region)
         {
-            return await _tourContext.Tours.Where(t => t.Region == region)
+            return await _tourContext.Tour.Where(t => t.Region == region)
                 .Select(t => t.Region)
                 .Distinct()
                 .ToListAsync();
@@ -65,7 +85,7 @@ namespace EPAM.DreamTour.DataAccess.Data
 
         public async Task<IEnumerable<TourModel>> GetTours()
         {
-            return await _tourContext.Tours.ToListAsync();
+            return await _tourContext.Tour.ToListAsync();
         }
     }
 }
